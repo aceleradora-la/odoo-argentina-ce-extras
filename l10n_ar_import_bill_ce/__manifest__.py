af8bd79fbb676d1b8a378e690af83a8664c00196
aceleradora-la <info@aceleradora.la>
2025-12-22 12:26:44 -0300

    Fix version number: Set to 17.0.1.0.0 for Odoo 17.0 branch

--- l10n_ar_import_bill_ce/__manifest__.py
+++ l10n_ar_import_bill_ce/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Argentinian Importing Bills from ARCA",
-    "version": "18.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Localization/Argentina",
     "sequence": 8,
     "author": "ADHOC SA",
