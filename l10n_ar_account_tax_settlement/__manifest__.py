b4ad53ac5d97919a4c6d7ed2bc91c29f3488586d
aceleradora-la <info@aceleradora.la>
2026-01-06 09:04:26 -0300

    Actualizar versiÃ³n a 18.0.1.0.0 en mÃ³dulos para branch 18.0
    - l10n_ar_account_reports: 17.0.1.0.0 -> 18.0.1.0.0
    - l10n_ar_account_tax_settlement: 17.0.1.0.0 -> 18.0.1.0.0
    - l10n_ar_import_bill_ce: 17.0.1.0.0 -> 18.0.1.0.0

--- l10n_ar_account_tax_settlement/__manifest__.py
+++ l10n_ar_account_tax_settlement/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Tax Settlements For Argentina (Community)",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
     "author": "ADHOC SA, Aceleradora-Latam",
     "website": "www.adhoc.com.ar",
